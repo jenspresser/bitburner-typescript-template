66fc3d1b4667531af7006bbe94d5f2e446ba2c35
Jens Presser <jens.presser@googlemail.com>
2023-12-04 19:54:53 +0100

    Script Buy Augmentations: Buy Neuroflux Governor

--- src/singularity/keepBuyingAugmentations.ts
+++ src/singularity/keepBuyingAugmentations.ts
@@ -1,32 +1,53 @@
 import { NS } from "@ns";
 import { getHomeServerMoney } from "/library";
 
+const NEUROFLUX_GOVERNOR = "NeuroFlux Governor";
+
 export async function main(ns: NS) {
+    ns.disableLog("ALL");
+    
     while(true) {
+        ns.clearLog();
+        
         purchaseAvailableAugmentations(ns);
 
-        await ns.sleep(5000);
+        await ns.sleep(2000);
     }
 }
 
 export function purchaseAvailableAugmentations(ns: NS) {
     let playerFactions = ns.getPlayer().factions;
 
+    ns.print("** Player factions: " + playerFactions);
+
     for(let faction of playerFactions) {
-        let availableAugs = ns.singularity.getAugmentationsFromFaction(faction);
-        let ownedAugs = ns.singularity.getOwnedAugmentations(true);
-        let reputation = ns.singularity.getFactionRep(faction);
-        
-        for(let aug of availableAugs) {
-            if(!ownedAugs.includes(aug)) {
+        ns.print("** Check for faction " + faction);
+
+        for(let aug of ns.singularity.getAugmentationsFromFaction(faction)) {
+            if(!ns.singularity.getOwnedAugmentations(true).includes(aug)) {
+                ns.print("Available from " + faction + ": " + aug);
+
                 let price = ns.singularity.getAugmentationPrice(aug);
                 let reputationRequired = ns.singularity.getAugmentationRepReq(aug);
+                let prerequisiteAugs = ns.singularity.getAugmentationPrereq(aug);
 
-                if(reputation > reputationRequired && getHomeServerMoney(ns) > price) {
-                    ns.singularity.purchaseAugmentation(faction, aug);
-                    ns.toast("Purchased Augmentation " + aug + " from " + faction);
-                    ns.print("Purchased Augmentation " + aug + " from " + faction);
+                let prerequisiteAugsValid = prerequisiteAugs.length === 0 || prerequisiteAugs.every((it) => ns.singularity.getOwnedAugmentations(true).includes(it));
+
+                if(prerequisiteAugsValid && ns.singularity.getFactionRep(faction) > reputationRequired && getHomeServerMoney(ns) > price) {
+                    if(ns.singularity.purchaseAugmentation(faction, aug)) {
+                        ns.toast("Purchased Augmentation " + aug + " from " + faction);
+                        ns.print("Purchased Augmentation " + aug + " from " + faction);
+                    }
                 } 
+            }
+        }
+
+        for(let i = 1; i<= 100; i++) {
+            if(ns.singularity.getAugmentationBasePrice(NEUROFLUX_GOVERNOR) <= getHomeServerMoney(ns)) {
+                if(ns.singularity.purchaseAugmentation(faction, NEUROFLUX_GOVERNOR)) {
+                    ns.toast("Purchased Augmentation " + NEUROFLUX_GOVERNOR + " from " + faction);
+                    ns.print("Purchased Augmentation " + NEUROFLUX_GOVERNOR + " from " + faction);
+                }
             }
         }
     }
