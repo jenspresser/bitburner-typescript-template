6d4c0a2ab3d8715397b62a63953235bd3715b96e
Jens Presser <jens.presser@googlemail.com>
2023-12-25 12:56:01 +0100

    Install Augmentations via Script

--- src/augmentations/augmentationsCallback.ts
+++ src/augmentations/augmentationsCallback.ts
@@ -2,5 +2,5 @@ import { NS } from "@ns";
 
 export async function main(ns: NS) {
     ns.tprint("### CALLBACK AFTER INSTALLING AUGMENTATIONS ###");
-    ns.exec("/status.js", "home", 1, "start", "adv", "--hn");
+    ns.exec("/status.js", "home", 1, "start", "adv", "--hn", "++tail");
 }
